90693802b7c07be63e93837e2431b0054fa24a4a
trimakichan <115846240+trimakichan@users.noreply.github.com>
2023-03-17 10:58:34 -0700

    Created a service and imported HttpClientModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     SearchComponent
   ],
   imports: [
