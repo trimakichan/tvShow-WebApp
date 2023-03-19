ffbcf56cfb11bcf3ec8127341000c54eb4ee0105
vuquanh <vuquanh@gmail.com>
2023-03-19 09:48:26 -0700

    Interface ITvShowData from API

---
+++ src/app/itv-show-data.ts
@@ -0,0 +1,11 @@
+export interface ITvShowData {
+    show: {
+        name: string,
+        genres: string,
+        rating: string,
+    },
+    image: {
+        medium: string
+    },
+    summary: string
+}
